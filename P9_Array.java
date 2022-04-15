a1b01639f75e4005e0b60fbc1bd096922c7990c1
Suhani220 <102501106+Suhani220@users.noreply.github.com>
2022-04-15 20:41:08 +0530

    Add files via upload

---
+++ P9_Array.java
@@ -0,0 +1,70 @@
+package Unit_01;
+
+/* Tasks given below:
+		obj.sortAnArray(array);
+		obj.findTheDuplicateElements(array);
+		obj.findSecondLargestAndSecondSmallestElement(array);
+		obj.leftRotationInAnArray(array); 
+		obj.removeElementInArray(array);//(Optional)
+ */
+
+public class P9_Array {
+
+	public static void main(String[] args) {
+
+		int[] array = { 1, 2, 3, 4, 5, 4, 6, 2, 3 };
+
+		QuestionsOnArray obj = new QuestionsOnArray();
+		obj.sortAnArray(array);
+//		obj.findTheDuplicateElements(array);
+//		obj.findSecondLargestAndSecondSmallestElement(array);
+//		obj.leftRotationInAnArray(array);
+//		obj.removeElementInArray(array); // (Optional)
+
+	}
+
+}
+
+class QuestionsOnArray {
+
+	void sortAnArray(int[] arr) {
+		// write code here!
+		int n = arr.length;
+		int[] b = new int[n];	
+		
+		for(int i=0; i<n; i++) {
+			int sn = arr[i];
+			for(int j=i; j<n; j++) {
+				if(sn >= arr[j]) {
+					sn = arr[j];
+				}
+			}
+			b[i] = sn;
+		}
+		
+		for(int i=0; i<n; i++) {
+			System.out.println(b[i]);
+		}
+	}
+
+	void findTheDuplicateElements(int[] arr) {
+		// write code here!
+
+	}
+
+	void findSecondLargestAndSecondSmallestElement(int[] arr) {
+		// write code here!
+
+	}
+
+	void leftRotationInAnArray(int[] arr) {
+		// write code here!
+
+	}
+
+	void removeElementInArray(int[] arr) {
+		// write code here!
+
+	}
+
+}
